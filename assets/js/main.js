63e746bd1efd72b8afec7bd90ed3840d10109412
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2024-10-31 21:59:26 +0100

    form toggle done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
 const stats_bar =document.getElementById("statisques-bar") 
 const stats = document.getElementById("statisques");
-const add_Task = document.getElementById("add-Task");
+const add_Task_btn = document.querySelectorAll(".add-Task-btn");
 
+const form = document.getElementById("task-Form");
+const form_title = document.getElementById("title");
+const description = document.getElementById("description");
+const due_date = document.getElementById("due-date");
+const due_hour = document.getElementById("due-time-hh");
+const due_minute = document.getElementById("due-time-mm");
+const cancel = document.getElementById("cancel");
+const done = document.getElementById("done");
+
+const high_p = document.getElementById("priority-high");
+const medium_p = document.getElementById("priority-medium");
+const low_p = document.getElementById("priority-low");
+
+
+//Form Toggle start
+add_Task_btn.forEach(button => {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        form.classList.remove("show-form");
+        console.log("Button clicked");
+    });
+});
+
+cancel.addEventListener("click", function(){
+    form.classList.add("show-form");
+})
+//Form Toggle end
+
+// change name of columns start
 

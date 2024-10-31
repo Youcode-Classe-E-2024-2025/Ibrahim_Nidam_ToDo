ed66aef572e0f26db3fb33e9d778e349e39b9c2d
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2024-10-31 13:02:29 +0100

    added the form for the project html / css

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const stats_bar =document.getElementById("statisques-bar") 
+const stats = document.getElementById("statisques");
+const add_Task = document.getElementById("add-Task");
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./*.html", // Main HTML file in the project root
-    "./assets/js/**/*.{js}", // All JavaScript files inside assets/js folder
+    // "./assets/js/**/*.{js}", // All JavaScript files inside assets/js folder
   ],
   theme: {
     extend: {
